820e8d4b9a4fc7946ee63c5f910cfde144b78044
arturdgmr <artur_dgmr@hotmail.com>
2021-08-15 15:47:54 -0300

    Ajusta classe PasswordValitionExecutorChainTest pra retornar o vo e não mais apenas boolean

--- src/test/java/br/com/artur/check/password/api/facade/password/PasswordValidateExecutorChainTest.java
+++ src/test/java/br/com/artur/check/password/api/facade/password/PasswordValidateExecutorChainTest.java
@@ -1,5 +1,6 @@
 package br.com.artur.check.password.api.facade.password;
 
+import br.com.artur.check.password.api.controller.vo.PasswordValidationVO;
 import br.com.artur.check.password.api.facade.password.validates.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -7,13 +8,14 @@ import org.junit.jupiter.api.Test;
 
 public class PasswordValidateExecutorChainTest {
 
-    /*private final BlankSpaceValidate blankSpaceValidate = new BlankSpaceValidate();
+    private final BlankSpaceValidate blankSpaceValidate = new BlankSpaceValidate();
     private final CharacterQuantityValidate characterQuantityValidate = new CharacterQuantityValidate();
     private final DigitsQuantityValidate digitsQuantityValidate = new DigitsQuantityValidate();
     private final LowerCaseQuantityValidate lowerCaseQuantityValidate = new LowerCaseQuantityValidate();
     private final RepeatedCharactersQuantityValidate repeatedCharactersQuantityValidate = new RepeatedCharactersQuantityValidate();
     private final SpecialCharacterQuantityValidate specialCharacterQuantityValidate = new SpecialCharacterQuantityValidate();
     private final UpperCaseQuantityValidate upperCaseQuantityValidate = new UpperCaseQuantityValidate();
+    private final PasswordValidatesFinallyStepOK passwordValidatesFinallyStepOK = new PasswordValidatesFinallyStepOK();
 
     PasswordValidateExecutorChain passwordValidateExecutorChain =
             new PasswordValidateExecutorChain(
@@ -23,55 +25,56 @@ public class PasswordValidateExecutorChainTest {
                     lowerCaseQuantityValidate,
                     repeatedCharactersQuantityValidate,
                     specialCharacterQuantityValidate,
-                    upperCaseQuantityValidate
+                    upperCaseQuantityValidate,
+                    passwordValidatesFinallyStepOK
             );
 
     @Test
     @DisplayName("should return false when password has blank space")
     public void blankSpaceValidateTest(){
         String passwd = "AbTp9 fok@";
-        Assertions.assertFalse(passwordValidateExecutorChain.executeValidations(passwd));
+        Assertions.assertEquals(passwordValidateExecutorChain.executeValidations(passwd), PasswordValidationVO.invalidPassword());
     }
 
     @Test
     @DisplayName("should return false when password is less than nine characters")
     public void characterQuantityValidateTest(){
         String passwd = "Abcd1@lk";
-        Assertions.assertFalse(passwordValidateExecutorChain.executeValidations(passwd));
+        Assertions.assertEquals(passwordValidateExecutorChain.executeValidations(passwd), PasswordValidationVO.invalidPassword());
     }
 
     @Test
     @DisplayName("should return false when the password has no digits")
     public void digitsQuantityValidateTest(){
         String passwd = "abcdefGH@!";
-        Assertions.assertFalse(passwordValidateExecutorChain.executeValidations(passwd));
+        Assertions.assertEquals(passwordValidateExecutorChain.executeValidations(passwd), PasswordValidationVO.invalidPassword());
     }
 
     @Test
     @DisplayName("should return false when the password has no lowercase letter")
     public void lowerCaseQuantityValidateTest(){
         String passwd = "ABCDEFGH@!1";
-        Assertions.assertFalse(passwordValidateExecutorChain.executeValidations(passwd));
+        Assertions.assertEquals(passwordValidateExecutorChain.executeValidations(passwd), PasswordValidationVO.invalidPassword());
     }
 
     @Test
     @DisplayName("should return false when password has repeated characters")
     public void repeatedCharactersQuantityValidateTest(){
         String passwd = "abcDEFGH@!1a";
-        Assertions.assertFalse(passwordValidateExecutorChain.executeValidations(passwd));
+        Assertions.assertEquals(passwordValidateExecutorChain.executeValidations(passwd), PasswordValidationVO.invalidPassword());
     }
 
     @Test
     @DisplayName("should return false when the password has no special characters")
     public void specialCharacterQuantityValidateTest(){
         String passwd = "abcDEFGH123";
-        Assertions.assertFalse(passwordValidateExecutorChain.executeValidations(passwd));
+        Assertions.assertEquals(passwordValidateExecutorChain.executeValidations(passwd), PasswordValidationVO.invalidPassword());
     }
 
     @Test
     @DisplayName("should return false when the password has no uppercase letter")
     public void upperCaseQuantityValidateValidateTest(){
         String passwd = "abcdefgh!@1";
-        Assertions.assertFalse(passwordValidateExecutorChain.executeValidations(passwd));
-    }*/
+        Assertions.assertEquals(passwordValidateExecutorChain.executeValidations(passwd), PasswordValidationVO.invalidPassword());
+    }
 }

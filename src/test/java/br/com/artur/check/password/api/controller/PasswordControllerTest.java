e16241a4921c2fdbec1cf2af5337de4949e47c5b
arturdgmr <artur_dgmr@hotmail.com>
2021-08-15 22:02:38 -0300

    Adiciona teste do PasswordController

--- src/test/java/br/com/artur/check/password/api/controller/PasswordControllerTest.java
+++ src/test/java/br/com/artur/check/password/api/controller/PasswordControllerTest.java
@@ -1,45 +1,40 @@
 package br.com.artur.check.password.api.controller;
 
-
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
-@WebAppConfiguration
+@SpringBootTest
 @AutoConfigureMockMvc
 public class PasswordControllerTest {
 
     @Autowired
-    private WebApplicationContext webApplicationContext;
-
     private MockMvc mockMvc;
 
-    private static final String URI = "/password/teste";
+    private static final String URI = "/password/validation";
 
-    @BeforeEach
-    public void setup() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    @Test
+    @DisplayName("Should return true for password validation and status 200")
+    public void getValidPassword() throws Exception {
+        String passwd = "ASDfghzxc@1";
+
+        mockMvc.perform(get(URI.concat("/"+passwd))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("isValid").value(true));
     }
 
-    /*@Test
-    public void givenHomePageURI_whenMockMVC_thenReturnsIndexJSPViewName() throws Exception {
-        mockMvc.perform(get(URI).param("passwd", "asd")).andExpect(status().isOk());
-    }*/
+
 
 }

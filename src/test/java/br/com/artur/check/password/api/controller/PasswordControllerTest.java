c8ec34dfa3d496d9314503da22b15ae578a294c1
arturdgmr <artur_dgmr@hotmail.com>
2021-08-15 22:31:43 -0300

    Adiciona mais cenarios de teste no PasswordControllerTest

--- src/test/java/br/com/artur/check/password/api/controller/PasswordControllerTest.java
+++ src/test/java/br/com/artur/check/password/api/controller/PasswordControllerTest.java
@@ -25,7 +25,7 @@ public class PasswordControllerTest {
     private static final String URI = "/password/validation";
 
     @Test
-    @DisplayName("Should return true for password validation and status 200")
+    @DisplayName("Should return true for password validation")
     public void getValidPassword() throws Exception {
         String passwd = "ASDfghzxc@1";
 
@@ -36,9 +36,64 @@ public class PasswordControllerTest {
     }
 
     @Test
-    @DisplayName("Should return false for password validation and status 200")
-    public void getInvalidPassword() throws Exception {
-        String passwd = "aa";
+    @DisplayName("Should return false for password no digits")
+    public void getPasswordWithoutDigits() throws Exception {
+        String passwd = "AbTp!fokz";
+
+        mockMvc.perform(get(URI.concat("/"+passwd))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("isValid").value(false));
+    }
+
+    @Test
+    @DisplayName("Should return false for password no lowercase letters")
+    public void getPasswordWithoutLowerCase() throws Exception {
+        String passwd = "ABTP!FOK1";
+
+        mockMvc.perform(get(URI.concat("/"+passwd))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("isValid").value(false));
+    }
+
+    @Test
+    @DisplayName("Should return false for password no special characters")
+    public void getPasswordWithoutSpecialCharacter() throws Exception {
+        String passwd = "AbTp9yfok";
+
+        mockMvc.perform(get(URI.concat("/"+passwd))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("isValid").value(false));
+    }
+
+    @Test
+    @DisplayName("Should return false for password no uppercase letters")
+    public void getPasswordWithoutUpperCase() throws Exception {
+        String passwd = "abtp9!fok";
+
+        mockMvc.perform(get(URI.concat("/"+passwd))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("isValid").value(false));
+    }
+
+    @Test
+    @DisplayName("Should return false for password with repeated character")
+    public void getPasswordWithRepeatedCharacter() throws Exception {
+        String passwd = "AbTp9!foA";
+
+        mockMvc.perform(get(URI.concat("/"+passwd))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("isValid").value(false));
+    }
+
+    @Test
+    @DisplayName("Should return false for password with insufficient number of characters")
+    public void getPasswordWithInsufficientNumberCharacters() throws Exception {
+        String passwd = "Tp9!foAx";
 
         mockMvc.perform(get(URI.concat("/"+passwd))
                         .accept(MediaType.APPLICATION_JSON))
